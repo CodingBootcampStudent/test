3f28934ab05b414d1937d1dc2fcb7be2ea5a36d1
jim <jim@jim-PC>
2017-12-19 19:15:15 +0200

    No2 commit

--- src/strategydesignpattern/Compressor.java
+++ src/strategydesignpattern/Compressor.java
@@ -5,17 +5,27 @@ package strategydesignpattern;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author jim
  */
 public interface Compressor {
-    
+
     public void compress(String text);
-    
+
     //falkjbsaflkjb
-    
+    fdslh ;
+    dadf sfddfh
+    dhf hds
+    dsf sh
+    fdsdf h                          
+    fdh gafe
+    fd f
+    df df
+                                                            
+    lsdk ;
+    laf
+                    
     
     
 }
